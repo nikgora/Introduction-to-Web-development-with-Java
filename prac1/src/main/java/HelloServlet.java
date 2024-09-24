4be908846e7125e3e162da50776cfe22b84b87f0
nikgora <nik.gora.2004@gmail.com>
2024-09-24 11:30:47 +0200

    prac2 done

--- prac1/src/main/java/HelloServlet.java
+++ prac1/src/main/java/HelloServlet.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.util.Enumeration;
+
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
@@ -12,13 +15,72 @@ public class HelloServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
+        HttpSession session = request.getSession();
+
+        session.setMaxInactiveInterval(5 * 60);
+
+        String user = (String) session.getAttribute("user");
+        if (user == null) {
+            user = request.getParameter("name");
+            if (user != null) {
+                session.setAttribute("user", user);
+            } else {
+                user = "Guest";
+            }
+        }
+
+        Cookie[] cookies = request.getCookies();
+        String visitCount = "0";
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("visitCount")) {
+                    visitCount = cookie.getValue();
+                }
+            }
+        }
+
+        int count = Integer.parseInt(visitCount) + 1;
+        Cookie visitCookie = new Cookie("visitCount", Integer.toString(count));
+        visitCookie.setMaxAge(60 * 60 * 24); // зберігається протягом одного дня
+        response.addCookie(visitCookie);
 
         // Hello
         String name = request.getParameter("name");
         PrintWriter out = response.getWriter();
+        ServletContext context = getServletContext();
 
         out.println("<html><body>");
         out.println("<h1>Hello, " + (name != null ? name : "World") + "!</h1>");
+
+        out.println("<h2>Servlet Context Information:</h2>");
+
+        Enumeration<String> initParams = context.getInitParameterNames();
+        if (initParams.hasMoreElements()) {
+            out.println("<p>Init Parameters:</p><ul>");
+            while (initParams.hasMoreElements()) {
+                String paramName = initParams.nextElement();
+                String paramValue = context.getInitParameter(paramName);
+                out.println("<li>" + paramName + " = " + paramValue + "</li>");
+            }
+            out.println("</ul>");
+        } else {
+            out.println("<p>No init parameters found.</p>");
+        }
+
+        Enumeration<String> attrNames = context.getAttributeNames();
+        if (attrNames.hasMoreElements()) {
+            out.println("<p>Context Attributes:</p><ul>");
+            while (attrNames.hasMoreElements()) {
+                String attrName = attrNames.nextElement();
+                Object attrValue = context.getAttribute(attrName);
+                out.println("<li>" + attrName + " = " + attrValue + "</li>");
+            }
+            out.println("</ul>");
+        } else {
+            out.println("<p>No context attributes found.</p>");
+        }
+
         out.println("</body></html>");
     }
 
